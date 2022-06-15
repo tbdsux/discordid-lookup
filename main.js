030c5a8b880dfdc2c674f403a745b9d888b8af84
TheBoringDude <iamcoderx@gmail.com>
2022-06-15 14:01:41 +0800

    done

--- main.js
+++ main.js
@@ -1,9 +1,28 @@
+import fetch from "cross-fetch";
 import express from "express";
 
 const app = express();
 
+const apiUrl = "https://discord.com/api/v10/users/";
+const TOKEN = process.env.TOKEN;
+
 app.get("/", (req, res) => {
   res.send("Hello");
+});
+
+app.get("/id/:id", async (req, res) => {
+  const { id } = req.params;
+
+  const r = await fetch(apiUrl + id, {
+    method: "GET",
+    headers: {
+      Authorization: "Bot " + TOKEN,
+    },
+  });
+
+  const response = await r.json();
+
+  res.status(200).json(response);
 });
 
 app.listen(3000, () => {
